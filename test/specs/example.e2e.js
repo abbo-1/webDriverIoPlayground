6fba912e0b3251a700a6f954d6f122aa832a7a4e
abbo-1 <abbott.kevinj@gmail.com>
2021-10-30 15:25:31 -0500

    added a test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,17 +1,30 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
+// describe('My Login application', () => {
+//     it('should login with valid credentials', async () => {
+//         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+//         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+//         await expect(SecurePage.flashAlert).toBeExisting();
+//         await expect(SecurePage.flashAlert).toHaveTextContaining(
+//             'You logged into a secure area!');
+//     });
+// });
+
+
+describe('webdriver.io page', () => {
+    it('should have the correct title', async () => {
+        browser.url('https://webdriver.io/')
+        await expect(browser).toHaveTitle(
+            'WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO'
+            );
     });
 });
 
-//
+//describe('NAME_OF_YOUR_TEST_COLLECTION', 
+
+//then comes a function, followed by an IT block. The it block is where your test is running. then your it function should have a name that tells you what you are trying to test.
+
 
 
